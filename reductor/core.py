85d76a6546cb10df716011dd7d5355d107f20040
Cstephen13 <carloesmax95@hotmail.com>
2019-07-08 20:38:50 -0500

    algunos cambios

--- reductor/core.py
+++ reductor/core.py
@@ -1,10 +1,23 @@
 import os
 from io import open
+from os import scandir, getcwd
+
+
+def ls(ruta = getcwd()):
+    lstFiles = []
+    lstDir = os.walk(ruta)
+    for root, dirs, files in lstDir:
+        for fichero in files:
+            nombreFichero, extension = os.path.splitext(fichero)
+            if extension == ".cnf":
+                lstFiles.append(nombreFichero)
+    return lstFiles
+
 
 try:
-    for index_archivo in range(100):
-        archivo = open("./instanciasSAT/"+str(index_archivo+1)+".cnf","r")
-        archivo_escrito = open("./instanciasMiniZinc/"+str(index_archivo+1)+".mzn","w")
+    for nombre_archivo in ls('../instanciasSAT/'):
+        archivo = open("../instanciasSAT/" + nombre_archivo + ".cnf", "r")
+        archivo_escrito = open("../instanciasMiniZinc/" + nombre_archivo + ".mzn", "w")
         for linea in map(lambda x: x.strip(), archivo.readlines()):
             if len(linea) > 0:
                 if linea[0] == 'p':
@@ -12,18 +25,17 @@ try:
                     num_variables = int(list_problem[2])
                     for data in range(num_variables*2):
                         if data >= num_variables:
-                            archivo_escrito.write("var 0..1: n_x_"+str(data-(num_variables-1))+";\n")
+                            archivo_escrito.write("var 0..1: n_x_" + str(data-(num_variables-1))+";\n")
                         else:
-                            archivo_escrito.write("var 0..1: x_"+str(data+1)+";\n")
+                            archivo_escrito.write("var 0..1: x_" + str(data+1)+";\n")
 
                     for data in range(num_variables):
-                        archivo_escrito.write("constraint x_"+str(data+1)+" + n_x_"+str(data+1)+" = 1;\n")
+                        archivo_escrito.write("constraint x_" + str(data+1) + " + n_x_" + str(data+1) + " = 1;\n")
 
                 if linea[0] != 'c' and linea[0] != 'p':
                     lista_vars = linea.split()
                     longitud_clausula = len(lista_vars)-1
                     restriccion = "constraint "
-
                     for i in range(len(lista_vars)-1):
                         valor_clausula = int(lista_vars[i])
                         if valor_clausula < 0:
